05ab1c98d078ce93ab5f1fb14263fac2fd561e8f
ido issaki <ido@gmail.com>
2026-03-11 20:00:51 +0200

    added pre_assembler.h

--- pre_assembler.h
+++ pre_assembler.h
@@ -4,16 +4,14 @@
 #define PRE_ASSEMBLER_H
 
 #include <stdio.h>
-#include "globals.h"
+#include "constant.h"
 
-/*
- *
- * 
- * 
- * 
- * 
- * 
+/* We pass the function a pointer to the file, the base file name (no extension),
+ * and a pointer to struct type "AssemberState"
+ * The purpose of this function is to read the original file context, (.as), 
+ * and find for macros, in order to create a new output file (.am), in that
+ * .am file, the macros will be switched and written as commands.
  */
-boolean pre_assemble(FILE *source_file, const char *base_file_name, AssemblerContext *context);
+boolean pre_assemble(FILE *source_file, char *base_file_name, AssemblerState *context);
 
 #endif
